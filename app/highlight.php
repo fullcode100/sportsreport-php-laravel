bf87c9a9e5979b518b881a95f0b77177f14f75f6
fullcode100 <snowleopard722@outlook.com>
2018-01-02 20:28:27 -0600

    Initial updates to use pagination on highlight feeds.

--- app/highlight.php
+++ app/highlight.php
@@ -8,16 +8,9 @@ class highlight extends Model
 {
     //
     public $timestamps = true;
-    private $number_of_post = 7;
-
-    public function getPrimaryFeed(){
-    	return $this->orderBy('highlight_id','desc')->take($this->number_of_post)->get();
-    }
 
     protected function postInTag($post_id_array){
     	return $this->wherein('highlight_id',$post_id_array)->orderby('highlight_id','DESC')->get();
     }
-
-    
 
 }
--- resources/views/homePageFeed.blade.php
+++ resources/views/homePageFeed.blade.php
@@ -15,13 +15,17 @@
 					<p>{!!$feed_item->highlight_description!!}</p>
 					<p>
 						<span class="share-btn">
-							<span tabindex="0" class="btn-link pointer-cursor" role="button" data-toggle="popover" data-trigger="focus" data-html="true" title="Share: <b>{{$feed_item->highlight_title}}</b>" data-content="<p>From: {{$feed_item->origin_url}}</p> <p>Here: {{ url('/') }}/{{$feed_item->highlight_id}}</p>">Share</span>
+							<span tabindex="0" class="btn-link pointer-cursor" role="button" data-toggle="popover" data-html="true" title="Share: <b>{{$feed_item->highlight_title}}</b>" data-content="<p>From: {{$feed_item->origin_url}}</p> <p>Here: {{ url('/') }}/{{$feed_item->highlight_id}}</p>">Share</span>
 							|
 						</span>
 						<a href="/highlight/{{$feed_item->highlight_id}}/{{$feed_item->url_slug}}/#comments">Comment</a> 
 					</p>
 				</div>
 			@endforeach
+		</div>
+
+		<div class="row">
+			{{ $feed_data->links() }}
 		</div>
 	</div>
 
